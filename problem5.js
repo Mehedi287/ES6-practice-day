5ac9b5191c4f8887d40471bb4ec21969f3886dcf
Mehedi390 <mh0816150@gmail.com>
2021-08-24 20:13:21 -0700

    problem 5 sloved

---
+++ problem5.js
@@ -0,0 +1,5 @@
+// onk gula number ar akta arry declair korvea . map ar maddore protea  arry ar Element ka 5 dea gun kore ar akra new arry ar moddea rakh bea pura kj ta one line hobea 
+
+const numbers = [12, 5, 21, 2, 22, 2, 22, 12, 12, 25, 25, 512, 5, 22, 15,];
+const newArr = numbers.map(n => n * 5);
+console.log(newArr);
