06682082c216e6fa02e6dfa4be5c7be0771c6b30
Mehedi390 <mh0816150@gmail.com>
2021-08-24 22:52:31 -0700

    problem 8 sloved

---
+++ problem8.js
@@ -0,0 +1,5 @@
+// problem 8 konu akta objact ar vlaue ka variable hesave declair korte hobe 
+
+const persons = { name: 'hasan', id: 87698, phoneNumber: 0174530256123 };
+const { name, id } = persons;
+console.log(name, id);
