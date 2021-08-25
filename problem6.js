b5bd3fef1437bbe431852db5cf470c698a99d900
Mehedi390 <mh0816150@gmail.com>
2021-08-24 22:12:08 -0700

    problem 6 sloved

---
+++ problem6.js
@@ -0,0 +1,6 @@
+// finde the all odd number from an arry using filter 
+
+
+const numbers = [4, 54, 52, 221, 655, 221, 653, 22, 3, 2, 323, 2, 32, 32, 3, 23, 232];
+const oddNumber = numbers.filter(n => n % 2);
+console.log(oddNumber);
