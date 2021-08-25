a35c8800136ca67d8dcde4b738a1f2d7ef327612
Mehedi390 <mh0816150@gmail.com>
2021-08-24 22:57:06 -0700

    problem 9 sloves

---
+++ problem9.js
@@ -0,0 +1,5 @@
+// problem 9 konu akta arry ar third element k variable hesabve  rakhbea vaiable ar name hovea three 
+
+const numbers = [45, 12, 45, 4542, 544, 2, 51];
+const [one, two, three] = numbers;
+console.log(three);
